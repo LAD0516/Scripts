9813016ab8f4d49bb0293bb5c483135f20080407
GnA1J <78709468+LAD0516@users.noreply.github.com>
2024-01-18 08:44:49 +0800

    更新 glados.js

--- QL/glados.js
+++ QL/glados.js
@@ -96,7 +96,7 @@ class UserInfo {
               "Origin": "https://glados.rocks",
 		    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299",		  
 			},
-	 body: `{"token": "glados.network"}`,
+	 body: `{"token": "glados.one"}`,
 
 };
 		if (Debug) {
